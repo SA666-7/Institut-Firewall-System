8709eadaa5a6212dcfa91b39142064c3321b9298
SA666-7 <sh666023@gmail.com>
2026-07-18 17:50:39 +0300

    Refactor SecurityRecommendation class
    Removed getter methods for recommendation and priority, and updated displayRecommendation method to format output.

--- src/SecurityRecommendation.java
+++ src/SecurityRecommendation.java
@@ -4,21 +4,17 @@ public class SecurityRecommendation {
     private String priority;
 
     public SecurityRecommendation(String recommendation, String priority) {
+
         this.recommendation = recommendation;
         this.priority = priority;
-    }
 
-    public String getRecommendation() {
-        return recommendation;
-    }
-
-    public String getPriority() {
-        return priority;
     }
 
     public void displayRecommendation() {
-        System.out.println("Security Recommendation:");
-        System.out.println("Recommendation: " + recommendation);
-        System.out.println("Priority: " + priority);
+
+        System.out.println("- " + recommendation
+                + " (Priority : " + priority + ")");
+
     }
+
 }
