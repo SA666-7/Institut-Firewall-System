88cedd6402bc8eb6b4b74274122ae1ef8c061fb1
SA666-7 <sh666023@gmail.com>
2026-07-20 23:18:50 +0300

    Refactor FirewallRule to use IP and port attributes

--- src/FirewallRule.java
+++ src/FirewallRule.java
@@ -1,38 +1,45 @@
 public class FirewallRule {
 
-    private String sourceDevice;
-    private String destinationDevice;
-    private String action;
+    private String sourceIp;
+    private String destinationIp;
+    private int port;
     private String protocol;
+    private String action;
 
-    public FirewallRule(String sourceDevice, String destinationDevice, String action, String protocol) {
-        this.sourceDevice = sourceDevice;
-        this.destinationDevice = destinationDevice;
-        this.action = action;
+    public FirewallRule(String sourceIp, String destinationIp, int port, String protocol, String action) {
+        this.sourceIp = sourceIp;
+        this.destinationIp = destinationIp;
+        this.port = port;
         this.protocol = protocol;
+        this.action = action;
     }
 
-    public String getSourceDevice() {
-        return sourceDevice;
+    public String getSourceIp() {
+        return sourceIp;
     }
 
-    public String getDestinationDevice() {
-        return destinationDevice;
+    public String getDestinationIp() {
+        return destinationIp;
     }
 
-    public String getAction() {
-        return action;
+    public int getPort() {
+        return port;
     }
 
     public String getProtocol() {
         return protocol;
     }
 
+    public String getAction() {
+        return action;
+    }
+
     public void displayRule() {
         System.out.println("Firewall Rule:");
-        System.out.println("From: " + sourceDevice);
-        System.out.println("To: " + destinationDevice);
-        System.out.println("Action: " + action);
+        System.out.println("Source IP: " + sourceIp);
+        System.out.println("Destination IP: " + destinationIp);
+        System.out.println("Port: " + port);
         System.out.println("Protocol: " + protocol);
+        System.out.println("Action: " + action);
     }
 }
